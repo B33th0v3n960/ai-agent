589a4fb441f931eeeb5a46e9435f6d0eac308db6
B33th0v3n960 <zhousun06@gmail.com>
2026-06-04 18:11:45 +0100

    feat: added verbose flag

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
-import argparse
+from google.genai import types
 
 
 def main():
@@ -15,16 +17,23 @@ def main():
     print(f"API key: {api_key}")
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
     prompt = args.user_prompt
-    print(f"User prompt: {prompt}")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
-    usage = response.usage_metadata 
-    if not usage:
-        raise RuntimeError("Failed API request.")
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    if (args.verbose):
+        print(f"User prompt: {prompt}")
+        usage = response.usage_metadata
+        if not usage:
+            raise RuntimeError("Failed API request.")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
 
     print(f"Response: \n {response.text}")
 
