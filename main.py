044cccc0939d2d6e4b949cf94dc3aa6afdc4f535
B33th0v3n960 <zhousun06@gmail.com>
2026-07-21 18:04:07 +0100

    chore: clean up main file

--- main.py
+++ main.py
@@ -6,31 +6,30 @@ from google import genai
 from google.genai import types
 
 
-def main():
+def main() -> None:
+    print("Hello from ai-agent!")
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("API key not found.")
-
     client = genai.Client(api_key=api_key)
-    print("Hello from ai-agent!")
-    print(f"API key: {api_key}")
+
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages: list[types.Content] = [
-        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-    ]
-    prompt = args.user_prompt
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages
     )
 
-    if (args.verbose):
+    if args.verbose:
+        prompt = args.user_prompt
         print(f"User prompt: {prompt}")
         usage = response.usage_metadata
-        if not usage:
+        if not response or not usage:
             raise RuntimeError("Failed API request.")
         print(f"Prompt tokens: {usage.prompt_token_count}")
         print(f"Response tokens: {usage.candidates_token_count}")
